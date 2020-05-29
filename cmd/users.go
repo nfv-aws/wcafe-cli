be2428128509c5b642a40674b98beae27eff9d29
ito-katsu <katsuma.ito.cp@nttcom.co.jp>
2020-05-29 05:56:41 +0000

    fix

--- cmd/users.go
+++ cmd/users.go
@@ -1,17 +1,13 @@
 package cmd
 
 import (
-	"context"
-	//	"crypto/rand"
-	//	"encoding/binary"
-	//	"encoding/json"
 	"bytes"
+	"context"
 	"fmt"
 	"io/ioutil"
 	"log"
-	//	math_rand "math/rand"
-	//	"net/http"
-	//	"strconv"
+	math_rand "math/rand"
+	"strconv"
 	"time"
 
 	//	"github.com/jmcvetta/napping"
@@ -120,7 +116,9 @@ func RunUsersCreateCmd(cmd *cobra.Command, args []string) error {
 // users createの処理
 func (client *Client) UserCreate(ctx context.Context) (string, error) {
 	subPath := fmt.Sprintf("/users")
-	body := `{"number": 12345678}`
+	math_rand.Seed(time.Now().UnixNano())
+	random_num := math_rand.Intn(10000)
+	body := `{"number":` + strconv.Itoa(random_num) + `}`
 	httpRequest, err := client.newRequest(ctx, "POST", subPath, bytes.NewBuffer([]byte(body)))
 	if err != nil {
 		log.Println("newRequest Error")
@@ -142,39 +140,3 @@ func (client *Client) UserCreate(ctx context.Context) (string, error) {
 
 	return string(res), nil
 }
-
-/*
-func runUsersCreateCmd(cmd *cobra.Command, args []string) error {
-	url := "http://" + dns + ":8080/api/v1/users"
-	log.Println("URL:>", url)
-
-	s := napping.Session{}
-	h := &http.Header{}
-	h.Set("X-Custom-Header", "myvalue")
-	s.Header = h
-
-	math_rand.Seed(time.Now().UnixNano())
-	random_num := math_rand.Intn(10000)
-
-	var jsonStr = []byte(`
-{
-    "number":` + strconv.Itoa(random_num) + `,
-    "name":"gogogo"
-}`)
-	var data map[string]json.RawMessage
-	err := json.Unmarshal(jsonStr, &data)
-	if err != nil {
-		log.Println(err)
-	}
-
-	resp, err := s.Post(url, &data, nil, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("response Status:", resp.Status())
-	log.Println("response Headers:", resp.HttpResponse().Header)
-	log.Println("response Body:", resp.RawText())
-
-	return nil
-}
-*/
