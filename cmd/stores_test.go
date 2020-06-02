dced2cf1063142d75e3da6a6afc29d2b3cf80d7e
EC2 Default User <ec2-user@ip-172-31-39-167.ap-northeast-1.compute.internal>
2020-06-02 05:55:57 +0000

    fix stores test

--- cmd/stores_test.go
+++ cmd/stores_test.go
@@ -9,62 +9,49 @@ import (
 )
 
 func TestStoreList(t *testing.T) {
-	cases := []struct {
-		body string
-	}{
-		{
-			body: `[
-		{"id":"sa5bafac-b35c-4852-82ca-b272cd79f2f3", "name":"Sano Shinichiro", "tag":"CLI",  "address":"Okinawa"},
-		{"id":"sa5bafac-b35c-4852-82ca-b272cd79f2f5", "name":"Suzuki Chihiro", "tag":"CLI",  "address":"Okinawa"}
-	]`,
-		},
-	}
+	body := `{
+		{"id": "sa5bafac-b35c-4852-82ca-b272cd79f2f3", "name": "Sano Shinichiro", "tag": "CLI", "address": "Okinawa"},
+		{"id": "sa5bafac-b35c-4852-82ca-b272cd79f2f5", "name": "Suzuki Chihiro", "tag": "CLI", "address": "Okinawa"},
+	}`
 
 	mux, mockServerURL := newMockServer()
 	client := newTestClient(mockServerURL)
 
-	for _, tc := range cases {
-		hundlePath := fmt.Sprintf("/api/stores")
-		mux.HandleFunc(hundlePath, func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprint(w, tc.body)
-		})
+	hundlePath := fmt.Sprintf("/api/stores")
 
-		list, err := client.StoreList(context.Background())
-		if err != nil {
-			t.Fatalf("StoreList was failed:list = %+v, err = %+v", list, err)
-		}
+	// mockのパターンをセット
+	mux.HandleFunc(hundlePath, func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, body)
+	})
 
-		if !reflect.DeepEqual(list, tc.body) {
-			t.Errorf("list = %+v, body = %+v", list, tc.body)
-		}
+	list, err := client.StoreList(context.Background())
+	if err != nil {
+		t.Fatalf("StoreList was failed:list = %+v, err = %+v", list, err)
+	}
+
+	if !reflect.DeepEqual(list, body) {
+		t.Errorf("list = %+v, body = %+v", list, body)
 	}
 }
 
 func TestStoreCreate(t *testing.T) {
-	cases := []struct {
-		body string
-	}{
-		{
-			body: `{"name":"Sano Shinichiro","tag":"CLI", "address":"Okinawa"}`,
-		},
-	}
+	body := `{"name":"Sano Shinichiro","tag":"CLI", "address":"Okinawa"}`
 
 	mux, mockServerURL := newMockServer()
 	client := newTestClient(mockServerURL)
+	hundlePath := fmt.Sprintf("/api/stores")
 
-	for _, tc := range cases {
-		hundlePath := fmt.Sprintf("/api/stores")
-		mux.HandleFunc(hundlePath, func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprint(w, tc.body)
-		})
+	// Mockパターンをセット
+	mux.HandleFunc(hundlePath, func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, body)
+	})
 
-		create, err := client.StoreCreate(context.Background())
-		if err != nil {
-			t.Fatalf("StoreCreate was failed:create = %+v, err = %+v", create, err)
-		}
+	create, err := client.StoreCreate(context.Background())
+	if err != nil {
+		t.Fatalf("StoreCreate was failed:create = %+v, err = %+v", create, err)
+	}
 
-		if !reflect.DeepEqual(create, tc.body) {
-			t.Errorf("create = %+v, body = %+v", create, tc.body)
-		}
+	if !reflect.DeepEqual(create, body) {
+		t.Errorf("create = %+v, body = %+v", create, body)
 	}
 }
